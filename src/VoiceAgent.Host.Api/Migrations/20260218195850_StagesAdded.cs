fbcf0d0513006304cf527854fb8c5bdc3d35beae
Abdul Sammad <Abdul Sammad@DESKTOP-L7JHC1H>
2026-02-19 01:22:29 +0500

    Changes in Migration

---
+++ src/VoiceAgent.Host.Api/Migrations/20260218195850_StagesAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VoiceAgent.Host.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class StagesAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CurrentStage",
+                table: "Calls",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentStage",
+                table: "Calls");
+        }
+    }
+}
