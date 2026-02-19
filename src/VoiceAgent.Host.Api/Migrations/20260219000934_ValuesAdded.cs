8298326d58236c5ee3a73334e85695da7cd2bb7c
Abdul Sammad <Abdul Sammad@DESKTOP-L7JHC1H>
2026-02-20 00:03:52 +0500

    Changes in Fields

---
+++ src/VoiceAgent.Host.Api/Migrations/20260219000934_ValuesAdded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VoiceAgent.Host.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ValuesAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FieldsJson",
+                table: "Calls",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FieldsJson",
+                table: "Calls");
+        }
+    }
+}
